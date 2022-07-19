2588d8b25467848173b5d6b9aff70fe4321854e3
Krash <108398610+FML-Krash@users.noreply.github.com>
2022-07-19 12:57:55 +0100

    Create main_class.java

---
+++ main_class.java
@@ -0,0 +1,10 @@
+package main_calculadora;
+
+public class main_calculadora {
+
+    public static void main(String[] args) {
+        Calculadora Calc = new Calculadora();
+        Calc.setVisible(true);
+    }
+    
+}
